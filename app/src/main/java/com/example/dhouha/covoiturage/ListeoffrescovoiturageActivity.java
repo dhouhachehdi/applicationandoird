38e038ba304983879c75ffa4b7975a43d1443749
dhouha chehdi <dhouha chehdi>
2016-05-03 10:22:47 +0100

    applicationandoird

--- app/src/main/java/com/example/dhouha/covoiturage/ListeoffrescovoiturageActivity.java
+++ app/src/main/java/com/example/dhouha/covoiturage/ListeoffrescovoiturageActivity.java
@@ -3,11 +3,11 @@ package com.example.dhouha.covoiturage;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ListedesoffresActivity extends AppCompatActivity {
+public class ListeoffrescovoiturageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_listedesoffres);
+        setContentView(R.layout.activity_listeoffrescovoiturage);
     }
 }
---
+++ app/src/main/java/com/example/dhouha/covoiturage/offrescovoituragesAdapter.java
@@ -0,0 +1,7 @@
+package com.example.dhouha.covoiturage;
+
+/**
+ * Created by Dhouha on 02/05/2016.
+ */
+public class offrescovoituragesAdapter {
+}
